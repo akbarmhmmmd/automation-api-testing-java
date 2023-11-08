802729dfb944c97b92d8e28d1a452931e8ad4cff
akbarmhmmmd <akbarmhmmmd@gmail.com>
2023-11-08 11:55:20 +0700

    feat: add DummyJSON request

---
+++ Scripts/Dummy JSON_TC/Get Products_TC/Script1699416291493.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+ResponseGetProduct = WS.sendRequest(findTestObject('Object Repository/DummyJSON/Get All Products'))
+
+WS.verifyResponseStatusCode(ResponseGetProduct, 200)
+
+WS.verifyElementPropertyValue(ResponseGetProduct, 'total', '100')
+
+WS.verifyElementPropertyValue(ResponseGetProduct, 'limit', '30')
+
+limitProducts = WS.getElementPropertyValue(ResponseGetProduct, 'limit')
+
+for (i = 0; i < limitProducts; i++) {
+	productId = WS.getElementPropertyValue(ResponseGetProduct, "products[${i}].id")
+	productName = WS.getElementPropertyValue(ResponseGetProduct, "products[${i}].name")
+	productPrice = WS.getElementPropertyValue(ResponseGetProduct, "products[${i}].price")
+	
+	WS.verifyElementPropertyValue(ResponseGetProduct, "products[${i}].id", productId)
+	WS.verifyElementPropertyValue(ResponseGetProduct, "products[${i}].name", productName)
+	WS.verifyElementPropertyValue(ResponseGetProduct, "products[${i}].price", productPrice)
+}
